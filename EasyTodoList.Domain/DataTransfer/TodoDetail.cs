cbdf6f261a7379c0729254ff9b8ef424330faf0c
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-14 18:58:02 -0500

    added todo detail dto, with method to populate it on the entity, in the domain

---
+++ EasyTodoList.Domain/DataTransfer/TodoDetail.cs
@@ -0,0 +1,15 @@
+﻿
+using EasyTodoList.Domain.Primitives;
+
+namespace EasyTodoList.Domain.DataTransfer;
+
+public class TodoDetail
+{
+    public required string Description { get; init; }
+    public required DateOnly? DueDate { get; init; }
+    public required bool IsImportant { get; init; }
+    public required bool IsComplete { get; init; }
+    public required DateTime CreateDate { get; init; }
+    public required DateTime? UpdateDate { get; init; }
+    public required Guid Id { get; init; }
+}
--- EasyTodoList.Domain/Entities/Todo.cs
+++ EasyTodoList.Domain/Entities/Todo.cs
@@ -27,6 +27,8 @@ internal class Todo : Entity
         Id = Identifier.Construct();
     }
 
+    public TodoDetail GetTodoDetail => new() { Description = Description.Value, DueDate = DueDate, IsImportant = IsImportant, IsComplete = IsComplete, CreateDate = Dates.CreateDate, UpdateDate = Dates.UpdateDate, Id = Id.Value };
+    
     private static Todo NotValid() => new("Input provided for todo is not valid.", null, false, false, DateTime.Now, null);
 
     public static Todo Construct(CreateTodo dto) => ValidateCreateTodo(dto).IsValid
