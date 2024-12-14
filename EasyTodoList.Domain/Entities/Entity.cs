e000734e30a6f736858590a52b2efd58994bd969
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-14 17:37:33 -0500

    added entities

---
+++ EasyTodoList.Domain/Entities/Entity.cs
@@ -0,0 +1,9 @@
+﻿
+using EasyTodoList.Domain.Primitives;
+
+namespace EasyTodoList.Domain.Entities;
+
+internal abstract class Entity
+{
+    internal abstract Identifier Id { get; init; }
+}
---
+++ EasyTodoList.Domain/Entities/Todo.cs
@@ -0,0 +1,29 @@
+﻿
+using EasyTodoList.Domain.Primitives;
+
+namespace EasyTodoList.Domain.Entities;
+
+internal class Todo : Entity
+{
+    internal Descriptor Description { get; init; }
+    internal DateOnly? DueDate { get; init; }
+    internal bool IsImportant { get; init; }
+    internal bool IsComplete { get; init; }
+    internal DateTimeStamps Dates { get; init; }
+    internal override Identifier Id { get; init; }
+
+    private Todo(string description, DateOnly? dueDate, bool isImportant, bool isComplete, DateTime createDate, DateTime? updateDate)
+    {
+        DateTimeStamps dates = DateTimeStamps.Construct(createDate, updateDate);
+
+        Description = Descriptor.Construct(description);
+        DueDate = dueDate;
+        IsImportant = isImportant;
+        IsComplete = isComplete;
+        Dates = dates;
+        Id = Identifier.Construct();
+    }
+
+    public static Todo Construct(string description, DateOnly? dueDate, bool isImportant, bool isComplete, DateTime createDate, DateTime? updateDate) =>
+        new(description, dueDate, isImportant, isComplete, createDate, updateDate);
+}
