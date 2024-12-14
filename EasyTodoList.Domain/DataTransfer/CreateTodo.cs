6f80d3568322b0da28a3d0d26f0e06de4201af0a
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-14 18:49:40 -0500

    added create todo dto to domain

---
+++ EasyTodoList.Domain/DataTransfer/CreateTodo.cs
@@ -0,0 +1,10 @@
+﻿
+namespace EasyTodoList.Domain.DataTransfer;
+
+public class CreateTodo
+{
+    public required string Description { get; init; }
+    public required DateOnly? DueDate { get; init; }
+    public required bool IsImportant { get; init; }
+    public required bool IsComplete { get; init; }
+}
--- EasyTodoList.Domain/Entities/Todo.cs
+++ EasyTodoList.Domain/Entities/Todo.cs
@@ -1,4 +1,5 @@
 ﻿
+using EasyTodoList.Domain.DataTransfer;
 using EasyTodoList.Domain.Primitives;
 using System.Diagnostics.CodeAnalysis;
 
@@ -26,6 +27,28 @@ internal class Todo : Entity
         Id = Identifier.Construct();
     }
 
-    public static Todo Construct(string description, DateOnly? dueDate, bool isImportant, bool isComplete, DateTime createDate, DateTime? updateDate) =>
-        new(description, dueDate, isImportant, isComplete, createDate, updateDate);
+    private static Todo NotValid() => new("Input provided for todo is not valid.", null, false, false, DateTime.Now, null);
+
+    public static Todo Construct(CreateTodo dto) => ValidateCreateTodo(dto).IsValid
+            ? new(dto.Description, dto.DueDate, dto.IsImportant, dto.IsComplete, DateTime.Now, null)
+            : Todo.NotValid();
+
+    private static (bool IsValid, string ErrorMessage) ValidateCreateTodo(CreateTodo dto)
+    {
+        bool isValid = true;
+        string error = string.Empty;
+
+        if (string.IsNullOrWhiteSpace(dto.Description))
+        {
+            isValid = false;
+            error = "Todo description is required and cannot consist of only whitespace charaters.";
+        }
+        else if (dto.Description.Length > 100)
+        {
+            isValid = false;
+            error = "Todo description must be 100 characters or fewer.";
+        }
+
+        return (isValid, error);
+    }
 }
