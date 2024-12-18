80181445dead86107bfa27564148a4db5b97a615
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-17 20:26:24 -0500

    added dto and mapper to domain

---
+++ EasyTodoList.Domain/DataTransfer/EntityDTOMapper.cs
@@ -0,0 +1,29 @@
+﻿
+using EasyTodoList.Domain.Entities;
+
+namespace EasyTodoList.Domain.DataTransfer;
+
+internal static class EntityDTOMapper
+{
+    internal static TodoDTO MapTodoEntityToDTO(Todo todo) =>
+        new()
+        {
+            Description = todo.Description.Value.Value,
+            DueDate = todo.DueDate,
+            IsImportant = todo.IsImportant,
+            IsComplete = todo.IsComplete,
+            CreateDate = todo.Dates.CreateDate,
+            UpdateDate = todo.Dates.UpdateDate,
+            Id = todo.Id.Value
+        };
+
+    internal static IEnumerable<TodoDTO> MapTodoEntitySequenceToTodoDTOSequence(IEnumerable<Todo> todos)
+    {
+        List<TodoDTO> dtos = [];
+        foreach (Todo todo in todos)
+        {
+            dtos.Add(MapTodoEntityToDTO(todo));
+        }
+        return dtos;
+    }
+}
---
+++ EasyTodoList.Domain/DataTransfer/TodoDTO.cs
@@ -0,0 +1,13 @@
+﻿
+namespace EasyTodoList.Domain.DataTransfer;
+
+internal class TodoDTO
+{
+    internal string Description { get; set; } = string.Empty;
+    internal DateOnly? DueDate { get; set; }
+    internal bool IsImportant { get; set; }
+    internal bool IsComplete { get; set; }
+    internal DateTime CreateDate { get; set; }
+    internal DateTime? UpdateDate { get; set; }
+    internal Guid Id { get; set; }
+}
