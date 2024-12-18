ac232d5adc6a97aece12e0d8fdbd04bb668b76b0
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-18 17:28:15 -0500

    added error message to descriptor constuct or throw in domain

--- EasyTodoList.Domain/Primitives/Descriptor.cs
+++ EasyTodoList.Domain/Primitives/Descriptor.cs
@@ -5,6 +5,6 @@ public record Descriptor(ValidString Value)
 {
     public static Descriptor ConstructOrThrowArgumentException(string value) =>
         string.IsNullOrWhiteSpace(value) || value.Length > 100
-        ? throw new ArgumentException(value)
+        ? throw new ArgumentException("Value is required, cannot consist of only whitespace characters, and must be 100 characters or fewer.", nameof(value))
         : new Descriptor(new ValidString(value));
 }
