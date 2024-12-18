e8d3f3676166f57b5752a54ca76ee33fa09b311d
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-17 20:28:48 -0500

    code clean up in todo domain entity

--- EasyTodoList.Domain/Entities/Todo.cs
+++ EasyTodoList.Domain/Entities/Todo.cs
@@ -1,5 +1,4 @@
 ﻿
-using EasyTodoList.Domain.DataTransfer;
 using EasyTodoList.Domain.Primitives;
 using System.Diagnostics.CodeAnalysis;
 
@@ -7,7 +6,6 @@ namespace EasyTodoList.Domain.Entities;
 
 public class Todo : Entity
 {
-    #region properties
     public required Descriptor Description { get; init; }
     public required DateOnly? DueDate { get; init; }
     public required bool IsImportant { get; init; }
@@ -15,62 +13,19 @@ public class Todo : Entity
     public required DateTimeStamps Dates { get; init; }
     public required override Identifier Id { get; init; }
 
-    public TodoDetail GetTodoDetail => new() { Description = Description.Value, DueDate = DueDate, IsImportant = IsImportant, IsComplete = IsComplete, CreateDate = Dates.CreateDate, UpdateDate = Dates.UpdateDate, Id = Id.Value };
-    #endregion properties
-
-    #region ctors
     private Todo() { }
 
     [SetsRequiredMembers]
     private Todo(string description, DateOnly? dueDate, bool isImportant, bool isComplete, DateTime createDate, DateTime? updateDate)
     {
-        Description = Descriptor.Construct(description);
+        Description = Descriptor.ConstructOrThrowArgumentException(description);
         DueDate = dueDate;
         IsImportant = isImportant;
         IsComplete = isComplete;
         Dates = DateTimeStamps.Construct(createDate, updateDate);
         Id = Identifier.Construct();
     }
-    #endregion ctors
 
-    #region factory methods
-    public static Todo Construct(CreateTodo dto)
-    {
-        (bool IsValid, string Error) = ValidateCreateTodo(dto);
-        return IsValid ? new(dto.Description, dto.DueDate, dto.IsImportant, dto.IsComplete, DateTime.Now, null) : Todo.NotValid(Error);
-    }
-
-    public static IEnumerable<Todo> ConstructEnumerable(IEnumerable<CreateTodo> dtos)
-    {
-        List<Todo> list = [];
-        foreach (CreateTodo dto in dtos)
-        {
-            list.Add(Todo.Construct(dto));
-        }
-        return list.AsEnumerable();
-    }
-
-    private static Todo NotValid(string error) => new(error, null, false, false, DateTime.Now, null);
-    #endregion factory methods
-
-    #region input validation
-    private static (bool IsValid, string ErrorMessage) ValidateCreateTodo(CreateTodo dto)
-    {
-        bool isValid = true;
-        string error = string.Empty;
-
-        if (string.IsNullOrWhiteSpace(dto.Description))
-        {
-            isValid = false;
-            error = "Todo description is required and cannot consist of only whitespace charaters.";
-        }
-        else if (dto.Description.Length > 100)
-        {
-            isValid = false;
-            error = "Todo description must be 100 characters or fewer.";
-        }
-
-        return (isValid, error);
-    }
-    #endregion input validation
+    internal static Todo Construct(string description, DateOnly? dueDate, bool isImportant, bool isComplete) =>
+        new(description, dueDate, isImportant, isComplete, DateTime.Now, null);
 }
