3551e9ef3f8d1f1bb5c0874586333395b9a93eb0
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-18 20:33:28 -0500

    added method to map dto properties to entity peoperties in domain

--- EasyTodoList.Domain/DataTransfer/EntityDTOMapper.cs
+++ EasyTodoList.Domain/DataTransfer/EntityDTOMapper.cs
@@ -26,4 +26,25 @@ public static class EntityDTOMapper
         }
         return dtos;
     }
+
+    public static Todo MapTodoDTOPropertiesToTodoEntityProperties(Todo entity, TodoDTO dto)
+    {
+        if (entity.Description.Value.Value != dto.Description)
+        {
+            entity.SetDescription(dto.Description);
+        }
+        if (entity.DueDate != dto.DueDate)
+        {
+            entity.SetDueDate(dto.DueDate);
+        }
+        if (entity.IsImportant != dto.IsImportant)
+        {
+            entity.SetIsImportant(dto.IsImportant);
+        }
+        if (entity.IsComplete != dto.IsComplete)
+        {
+            entity.SetIsComplete(dto.IsComplete);
+        }
+        return entity;
+    }
 }
