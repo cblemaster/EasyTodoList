33c08f2bff3f8200c19895b873005ac8a4455973
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-18 20:34:40 -0500

    made todo entity property setters private and added methods to set these values

--- EasyTodoList.Domain/Entities/Todo.cs
+++ EasyTodoList.Domain/Entities/Todo.cs
@@ -1,21 +1,18 @@
 ﻿
 using EasyTodoList.Domain.ValueObjects;
-using System.Diagnostics.CodeAnalysis;
 
 namespace EasyTodoList.Domain.Entities;
 
 public class Todo : Entity
 {
-    public required Descriptor Description { get; init; }
-    public DateOnly? DueDate { get; init; }
-    public bool IsImportant { get; init; }
-    public bool IsComplete { get; init; }
+    public Descriptor Description { get; private set; }
+    public DateOnly? DueDate { get; private set; }
+    public bool IsImportant { get; private set; }
+    public bool IsComplete { get; private set; }
     public DateTimeStamps Dates { get; init; }
     public override Identifier Id { get; init; }
 
-    private Todo() { }
-
-    [SetsRequiredMembers]
+    private Todo() { }  // this private ctor is only here for ef core, so I'm ok with the unresolved warning
     private Todo(string description, DateOnly? dueDate, bool isImportant, bool isComplete)
     {
         Description = Descriptor.ConstructOrThrowArgumentException(description);
@@ -26,6 +23,10 @@ public class Todo : Entity
         Id = new Identifier(Value: Guid.CreateVersion7());
     }
 
-    internal static Todo Construct(string description, DateOnly? dueDate, bool isImportant, bool isComplete) =>
+    public static Todo Construct(string description, DateOnly? dueDate, bool isImportant, bool isComplete) =>
         new(description, dueDate, isImportant, isComplete);
+    internal void SetDescription(string description) => Description = Descriptor.ConstructOrThrowArgumentException(description);
+    internal void SetDueDate(DateOnly? dueDate) => DueDate = dueDate;
+    internal void SetIsImportant(bool isImportant) => IsImportant = isImportant;
+    internal void SetIsComplete(bool isComplete) => IsComplete = isComplete;
 }
